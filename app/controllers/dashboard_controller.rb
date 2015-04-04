43527d91135c4fd34e3a08877cee3b5584385677
Hidekazu Kobayashi <kobahide789@gmail.com>
2015-04-04 14:47:38 +0000

    Impl dashboard for now

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   def index
+    @projects = [{
+                   _type: "project",
+                   id: "",
+                   name: "tkbctf6"
+                 }]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def path_to(arg)
+    if arg.is_a? Symbol
+      "/#{arg.to_s.pluralize}"
+    else
+      "/#{arg[:_type].pluralize}/#{arg[:id]}"
+    end
+  end
 end
